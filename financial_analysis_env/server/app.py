b017212443cedb83485cd1ca31e4aa4baf76fde9
Ishika Tyagi <ishika.tyagi@mastersunion.org>
2026-04-08 12:49:04 +0530

    dependencies

--- financial_analysis_env/server/app.py
+++ financial_analysis_env/server/app.py
@@ -1,4 +1,14 @@
 from openenv.core.env_server import create_fastapi_app
+# Import your environment and your two model classes
+from financial_analysis_env.models import FinancialAnalysisAction, FinancialAnalysisObservation
 from financial_analysis_env.environment import FinancialAnalysisEnvironment
 
-app = create_fastapi_app(FinancialAnalysisEnvironment)
+# 1. Create an instance of your environment
+env = FinancialAnalysisEnvironment()
+
+# 2. Pass the instance AND the two classes to the helper function
+app = create_fastapi_app(
+    env, 
+    action_cls=FinancialAnalysisAction, 
+    observation_cls=FinancialAnalysisObservation
+)
